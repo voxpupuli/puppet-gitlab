30e28fb68bd22e7f2f0830e732224fbd34430703
Tobias Brunner <tobias.brunner@vshn.ch>
2015-07-13 18:44:47 +0200

    more spec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -53,4 +53,58 @@ describe 'gitlab' do
       it { expect { is_expected.to contain_package('gitlab') }.to raise_error(Puppet::Error, /OS family Solaris not supported/) }
     end
   end
+
+  context 'gitlab specific parameters' do
+    let(:facts) {{
+      :osfamily => 'debian',
+      :lsbdistid => 'debian',
+      :lsbdistcodename => 'jessie',
+    }}
+    it { is_expected.to contain_file('/etc/gitlab/gitlab.rb') }
+    describe 'edition = ce' do
+      let(:params) { {:edition => 'ce'} }
+      it { is_expected.to contain_package('gitlab-ce').with_ensure('installed') }
+    end
+    describe 'edition = ee' do
+      let(:params) { {:edition => 'ee'} }
+      it { is_expected.to contain_package('gitlab-ee').with_ensure('installed') }
+    end
+    describe 'external_url' do
+      let(:params) { {:external_url => 'http://gitlab.mycompany.com/'} }
+      it { is_expected.to contain_file('/etc/gitlab/gitlab.rb') \
+        .with_content(/^\s*external_url 'http:\/\/gitlab\.mycompany\.com\/'$/)
+      }
+    end
+    describe 'ci_external_url' do
+      let(:params) { {:ci_external_url => 'http://gitlabci.mycompany.com/'} }
+      it { is_expected.to contain_file('/etc/gitlab/gitlab.rb') \
+        .with_content(/^\s*ci_external_url 'http:\/\/gitlabci\.mycompany\.com\/'$/)
+      }
+    end
+    describe 'nginx' do
+      let(:params) { {:nginx => {
+        'enable' => true,
+        'listen_port' => 80,
+        }
+      }}
+      it { is_expected.to contain_file('/etc/gitlab/gitlab.rb') \
+        .with_content(/^\s*nginx\['enable'\] = true$/)
+        .with_content(/^\s*nginx\['listen_port'\] = '80'$/)
+      }
+      describe 'and ci_nginx_eq_nginx = true' do
+        let(:params) { {:nginx => {
+          'enable' => true,
+          'listen_port' => 80,
+          },
+          :ci_nginx_eq_nginx => true,
+        }}
+        it { is_expected.to contain_file('/etc/gitlab/gitlab.rb') \
+          .with_content(/^\s*nginx\['enable'\] = true$/)
+          .with_content(/^\s*nginx\['listen_port'\] = '80'$/)
+          .with_content(/^\s*ci_nginx\['enable'\] = true$/)
+          .with_content(/^\s*ci_nginx\['listen_port'\] = '80'$/)
+        }
+      end
+    end
+  end
 end

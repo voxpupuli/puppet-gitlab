18f38eff33a5ac800b3f448abd3b02f5ee10a23c
Walter Heck <walterheck@olindata.com>
2015-12-16 10:28:12 +0100

    remove accidental duplicate resource from gitlab::user define
    Forgot to remove the old exec resource in the defined type gitlab::user

--- manifests/user.pp
+++ manifests/user.pp
@@ -6,15 +6,6 @@ define gitlab::user(
   ) {
   
   validate_string($username, $email, $password, $fullname)
-
-  exec { "create-user-${title}":
-    command => "source ~/.gitlab; /usr/local/bin/gitlab '${email}' '${password}' '${username}' '{name: \"${fullname}\"}'",
-    onlyif  => [ 
-      "source ~/.gitlab; /usr/local/bin/gitlab users | grep $email",
-      "source ~/.gitlab; /usr/local/bin/gitlab users | grep $username",
-    ],
-    require => Package['gitlab-cli'],
-  }
   
   exec { "create-user-${title}":
     command => "/bin/bash -c 'source /root/.gitlab; /usr/local/bin/gitlab create_user \"${email}\" \"${password}\" \"${username}\" \"{name: \\\"${fullname}\\\"}\"'",

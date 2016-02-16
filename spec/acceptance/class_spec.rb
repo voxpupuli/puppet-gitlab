b29147dfd80a348db40c0e04ec0112210246fe15
Peter Souter <p.morsou@gmail.com>
2016-02-16 16:13:28 +0000

    Fixes class spec to working config
    Also adds test to actually check Gitlab is runnign

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -6,7 +6,9 @@ describe 'gitlab class' do
     # Using puppet_apply as a helper
     it 'should work idempotently with no errors' do
       pp = <<-EOS
-      class { 'gitlab': }
+      class { 'gitlab':
+        external_url => "http://${::fqdn}",
+      }
       EOS
 
       # Run it twice and test for idempotency
@@ -19,8 +21,15 @@ describe 'gitlab class' do
     end
 
     describe service('gitlab') do
-      it { is_expected.to be_enabled }
       it { is_expected.to be_running }
     end
+
+    it 'allows http connection on port 8080' do
+      shell 'sleep 15' # give it some time to start up
+      describe command('curl 0.0.0.0:80/users/sign_in') do
+        its(:stdout) { should match /GitLab/ }
+      end
+    end
+
   end
 end

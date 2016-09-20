6a41290f51cafb55b3187e36f061f03d525e34ed
Peter Souter <p.morsou@gmail.com>
2016-09-20 10:26:17 +0100

    Refactor beaker tests for Travis
    * There are issues running docker in docker in Travis, so lets test the easiest use-case
    * Adds a specific Travis spec with the docker workarounds required
    * Revert the class spec so tests in Vagrant can still work outside of Travis

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -7,8 +7,7 @@ describe 'gitlab class' do
     it 'should work idempotently with no errors' do
       pp = <<-EOS
       class { 'gitlab':
-        external_url   => "http://${::fqdn}",
-        service_manage => false,
+        external_url => "http://${::fqdn}",
       }
       EOS
 
@@ -17,37 +16,15 @@ describe 'gitlab class' do
       apply_manifest(pp, :catch_changes  => true)
     end
 
-    it 'should run reconfigure when config changes' do
-
-      # gitlab-omnibus works differently in docker
-      # Requires manual kick for beaker docker tests
-      docker_workaround = <<-EOS
-      exec { '/opt/gitlab/embedded/bin/runsvdir-start &':
-        onlyif => '/bin/cat /proc/self/cgroup | grep docker'
-      }
-      EOS
-
-      apply_manifest(docker_workaround, :catch_failures => true)
-
-      start_service_pp = <<-EOS
-      class { 'gitlab':
-        external_url   => "http://${::fqdn}",
-        gitlab_rails   => { 'time_zone' => 'GMT' }
-      }
-      EOS
-
-      apply_manifest(start_service_pp, :catch_failures => true, :show_diff => true) do |r|
-        expect(r.stdout).to match(/Scheduling refresh of Exec\[gitlab_reconfigure\]/)
-      end
-    end
-
     describe package('gitlab-ce') do
       it { is_expected.to be_installed }
     end
 
-    context 'allows http connection on port 80' do
+
+    it 'allows http connection on port 8080' do
+      shell 'sleep 15' # give it some time to start up
       describe command('curl 0.0.0.0:80/users/sign_in') do
-        its(:stdout) { should match /reset_password_token|GitLab/ }
+        its(:stdout) { should match /GitLab|password/ }
       end
     end
 

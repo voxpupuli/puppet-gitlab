7d318fbbf505597a05ff48e6dc818226e98e2cfd
Tobias Brunner <tobias@tobru.ch>
2016-10-07 10:10:34 +0200

    use top scope for fact

--- manifests/params.pp
+++ manifests/params.pp
@@ -39,7 +39,7 @@ class gitlab::params {
   }
 
   # gitlab specific
-  $external_url = "http://${fqdn}"
+  $external_url = "http://${::fqdn}"
   $config_manage = true
   $config_file = '/etc/gitlab/gitlab.rb'
   $secrets_file = '/etc/gitlab/gitlab-secrets.json'

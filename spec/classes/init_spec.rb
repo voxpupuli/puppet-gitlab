b23876ad0e013f78bbcdabbb18e6338ffb12f601
Brian Vanderbusch <brian.vanderbusch.qsho@statefarm.com>
2018-04-03 08:37:01 -0500

    new roles regex in spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -219,9 +219,13 @@ describe 'gitlab', type: :class do
           it { is_expected.not_to contain_package('gitlab-ee') }
         end
         describe 'with roles' do
-          let(:params) { { :roles => ['redis_sentinel_role', 'redis_master_role'] } }
+          let(:params) do
+            {
+              'roles' => ['redis_sentinel_role', 'redis_master_role']
+            }
+          end
           it { is_expected.to contain_file('/etc/gitlab/gitlab.rb') \
-            .with_content(/^\s*roles \["redis_sentinel_role", "redis_master_role"\]$/)
+            .with_content(%r{\s*roles\ %w[redis_sentinel_role redis_master_role]})
           }
         end
         describe 'with data_dirs' do

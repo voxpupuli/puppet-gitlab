de0ba9d039c3888778533a269281de2c64ef7837
Brian Vanderbusch <brian.vanderbusch.qsho@statefarm.com>
2018-05-25 09:48:26 -0500

    formatting fixes

--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -11,16 +11,17 @@ class gitlab::preinstall (
   # Deprecation notice:
   # skip_auto_migrations is deprecated and will be removed at some point after
   # GitLab 11.0 is released
-	$skip_auto_migrations_deprecation_msg = "DEPRECTATION: 'skip_auto_migrations' is deprecated if using GitLab 10.6 or greater. Set skip_auto_reconfigure instead"
+  $skip_auto_migrations_deprecation_msg = "DEPRECTATION: 'skip_auto_migrations' is deprecated if using GitLab 10.6 or greater. Set skip_auto_reconfigure instead"
 
   if $skip_auto_migrations != undef {
 
-		notify { $skip_auto_migrations_deprecation_msg: }
+    notify { $skip_auto_migrations_deprecation_msg: }
 
     $_skip_auto_migrations_ensure = $skip_auto_migrations ? {
       true    => 'present',
       default => 'absent',
     }
+
     file { '/etc/gitlab/skip-auto-migrations':
       ensure => $_skip_auto_migrations_ensure,
       owner  => 'root',

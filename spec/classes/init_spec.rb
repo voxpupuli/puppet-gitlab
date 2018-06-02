9ccd843999373dd5c306336f6f4ddfd49d556270
Brian Vanderbusch <chief@hackerhappyhour.com>
2018-06-02 18:05:47 -0500

    fix spec for service subscriptions

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -12,8 +12,8 @@ describe 'gitlab', type: :class do
         it { is_expected.to contain_class('gitlab::host_config').that_comes_before('Class[gitlab::install]') }
         it { is_expected.to contain_class('gitlab::omnibus_config').that_comes_before('Class[gitlab::install]') }
         it { is_expected.to contain_class('gitlab::install').that_comes_before('Class[gitlab::service]') }
-        it { is_expected.to contain_class('gitlab::service').that_subscribes_to('Class[gitlab::omnibus_config]') }
-        it { is_expected.to contain_exec('gitlab_reconfigure') }
+        it { is_expected.to contain_class('gitlab::service') }
+        it { is_expected.to contain_exec('gitlab_reconfigure').that_subscribes_to('Class[gitlab::omnibus_config]') }
         it { is_expected.to contain_file('/etc/gitlab/gitlab.rb') }
         it { is_expected.to contain_service('gitlab-runsvdir') }
         it { is_expected.to contain_package('gitlab-ce').with_ensure('installed') }

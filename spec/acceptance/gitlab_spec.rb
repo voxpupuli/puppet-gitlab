b6fdac8a57a468b9df5ec19ca7929d2e4da77253
Romain Tartière <romain@blogreen.org>
2018-12-13 07:16:58 -1000

    Sleep after applying manifests
    We sleep 15 seconds to wait for GitLab to finish starting up, but because the
    sleep is outside of any test case / context, it happen to be executed _before_
    manifests to install GitLab are actually applied.
    
    Move the sleep at the end of the first step of the test suite.

--- spec/acceptance/gitlab_spec.rb
+++ spec/acceptance/gitlab_spec.rb
@@ -11,13 +11,13 @@ describe 'gitlab class' do
 
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
+
+      shell('sleep 15') # give it some time to start up
     end
 
     describe package('gitlab-ce') do
       it { is_expected.to be_installed }
     end
-
-    shell('sleep 15') # give it some time to start up
 
     describe command('curl -s -S 0.0.0.0:80/users/sign_in') do
       its(:stdout) { is_expected.to match %r{.*reset_password_token=.*redirected.*} }

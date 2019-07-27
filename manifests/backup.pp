d59422fe0a9c6da5cb32969c6bd34103b91e2d1f
Tim Meusel <tim@bastelfreak.de>
2019-07-27 14:48:57 +0200

    puppet-lint: fix topscope_variable

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -3,14 +3,14 @@
 # This class is called from gitlab for backup config.
 #
 class gitlab::backup {
-  $rake_exec = $::gitlab::rake_exec
-  $backup_cron_enable = $::gitlab::backup_cron_enable
-  $backup_cron_minute = $::gitlab::backup_cron_minute
-  $backup_cron_hour = $::gitlab::backup_cron_hour
-  if empty($::gitlab::backup_cron_skips) {
+  $rake_exec = $gitlab::rake_exec
+  $backup_cron_enable = $gitlab::backup_cron_enable
+  $backup_cron_minute = $gitlab::backup_cron_minute
+  $backup_cron_hour = $gitlab::backup_cron_hour
+  if empty($gitlab::backup_cron_skips) {
     $backup_cron_skips = ''
   } else {
-    $_backup_cron_skips = join($::gitlab::backup_cron_skips, ',')
+    $_backup_cron_skips = join($gitlab::backup_cron_skips, ',')
     $backup_cron_skips = "SKIP=${_backup_cron_skips}"
   }
 

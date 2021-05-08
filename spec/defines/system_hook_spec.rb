48cc3668de0b60dce0311b15a7c242ee0ce3183d
Carlos Duelo <c@cduelo.com>
2021-05-08 09:49:06 -0500

    Add system hooks management (#380)
    * Add system hooks management
    
    A file hook will run on each event so it's up to you to filter events
    or projects within a file hook code. You can have as many file hooks
    as you want. Each file hook will be triggered by GitLab asynchronously
    in case of an event. For a list of events see the system hooks
    documentation.
    Reference: http://10.191.3.4/help/administration/file_hooks
    
    * add test for system hook
    
    * limit it to absolute paths
    
    Co-authored-by: Tim Meusel <tim@bastelfreak.de>
    
    * ensure that the whole catalog compiles
    
    Co-authored-by: Tim Meusel <tim@bastelfreak.de>
    
    * make stricter sytem_hooks parameter
    
    Co-authored-by: Tim Meusel <tim@bastelfreak.de>
    
    * fix resource name
    
    * set default in the init.pp for system_hook
    
    * set default in the init.pp for global_hook
    
    * set the correct type to system_hook in init.pp
    
    * fix system_hook tests
    
    * remove no necesary optinal and use default value
    
    Co-authored-by: Tim Meusel <tim@bastelfreak.de>

---
+++ spec/defines/system_hook_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe 'gitlab::system_hook' do
+  let(:title) { 'test-system-hook' }
+
+  let(:pre_condition) do
+    <<-MANIFEST
+    class { 'gitlab':
+      repository_configuration => {},
+    }
+    MANIFEST
+  end
+
+  context "with source" do
+    let(:source) { 'puppet:///modules/my_module/file-hook' }
+    let(:params) do
+      {
+        system_hooks_dir: '/custom/hooks/dir',
+        source: source
+      }
+    end
+
+    it { is_expected.to compile.with_all_deps }
+
+    it do
+      is_expected.to contain_file("/custom/hooks/dir").
+        with_ensure('directory')
+    end
+
+    it do
+      is_expected.to contain_file("/custom/hooks/dir/#{title}").
+        with_ensure('file').
+        with_source(source)
+    end
+  end
+
+  context "with source" do
+    let(:content) { "#!/usr/bin/env bash\ntest 0" }
+    let(:params) do
+      {
+        system_hooks_dir: '/custom/hooks/dir',
+        content: content
+      }
+    end
+
+    it { is_expected.to compile.with_all_deps }
+
+    it do
+      is_expected.to contain_file("/custom/hooks/dir").
+        with_ensure('directory')
+    end
+
+    it do
+      is_expected.to contain_file("/custom/hooks/dir/#{title}").
+        with_ensure('file').
+        with_content(content)
+    end
+  end
+end

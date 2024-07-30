2e1738636e584a181972a15d628251a23177a3e7
jbond <github@johnbond.org>
2024-07-30 11:48:05 +0200

    cirunner: update to support multiple runners
    Bug: DNSIN-258

--- manifests/cirunner.pp
+++ manifests/cirunner.pp
@@ -1,34 +1,34 @@
 # @summary This module installs and configures Gitlab CI Runners.
-# @param concurrent the number of concurrent jobs the runner can handle
 # @param conf_file path to the config file
-# @param token the runner registration token
 # @param url the url to the gitlab server
+# @param runners the hash of runners
 #
 class gitlab::cirunner (
-  Integer          $concurrent = 1,
-  String           $conf_file  = '/etc/gitlab-runner/config.toml',
-  Optional[String] $token      = undef,
-  String           $url        = 'https://gitlab.com',
+  String               $conf_file  = '/etc/gitlab-runner/config.toml',
+  String               $url        = 'https://gitlab.com',
+  Hash[String, String] $runners = {},
 ) {
+  $concurrent = $runners.size
   $package_name = 'gitlab-runner'
   ensure_packages([$package_name])
-  $command = "/usr/bin/gitlab-ci-multi-runner register -n --executor shell --token ${token} --url ${url}"
 
   exec { 'gitlab-runner-restart':
     command     => "/usr/bin/${package_name} restart",
     refreshonly => true,
     require     => Package[$package_name],
   }
-  exec { 'Register_runner':
-    command => $command,
-    unless  => "/bin/grep ${token} ${conf_file}",
-    require => Package[$package_name],
+  $runners.each |$name, $token| {
+    $command = "/usr/bin/gitlab-ci-multi-runner register -n --executor shell --token ${token} --url ${url}"
+    exec { "Register_runner ${name}":
+      command => $command,
+      unless  => "/bin/grep ${token} ${conf_file}",
+      require => Package[$package_name],
+    }
   }
   file_line { 'gitlab-runner-concurrent':
-    path    => $conf_file,
-    line    => "concurrent = ${concurrent}",
-    match   => '^concurrent = \d+',
-    require => Exec['Register_runner'],
-    notify  => Exec['gitlab-runner-restart'],
+    path   => $conf_file,
+    line   => "concurrent = ${concurrent}",
+    match  => '^concurrent = \d+',
+    notify => Exec['gitlab-runner-restart'],
   }
 }
